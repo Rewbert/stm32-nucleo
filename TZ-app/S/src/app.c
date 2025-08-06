348c43a0f67ed78a6620e3904f4eedc6405adfe6
Rewbert <krookr@chalmers.se>
2025-08-06 10:51:09 +0200

    blink the LED from secure code

--- TZ-app/S/src/app.c
+++ TZ-app/S/src/app.c
@@ -3,7 +3,43 @@
 #define ENABLE_IRQ()  __asm volatile ("cpsie i" : : : "memory")
 #define DISABLE_IRQ() __asm volatile ("cpsid i" : : : "memory")
 
+void configure_red_led(void) {
+  volatile int dummy;
+  RCC_S->AHB2ENR |= (1 << RCC_AHB2ENR_GPIOAEN_Pos);
+  dummy = RCC_S->AHB2ENR;
+  dummy = RCC_S->AHB2ENR;
+
+  GPIOA_S->MODER &= ~(GPIO_MODER_MODE9_Msk);
+  GPIOA_S->MODER |= (1 << GPIO_MODER_MODE9_Pos);
+}
+
+volatile uint32_t ticks;
+void systick_handler() {
+  ticks++;
+}
+
+void delay_ms(uint32_t milliseconds) {
+  uint32_t start = ticks;
+  uint32_t end = start + milliseconds;
+
+  if (end < start) {
+      while (ticks > start);
+  }
+  while(ticks < end);
+}
+
 void secure_app_initialise() {
+  configure_red_led();
+
+  GPIOA_S->ODR |= (1 << 9);
+
+  SysTick_Config(4000);
+  ENABLE_IRQ();
+
+  while(1) {
+    GPIOA_S->ODR ^= (1 << 9);
+    delay_ms(500);
+  }
     // configure_clock();
     // SysTick_Config2(110000);
 
