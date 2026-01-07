f2ceb98b435a4d87f4cc80f6ea88ef33f91e36a3
Rewbert <krookr@chalmers.se>
2026-01-07 15:06:55 +0100

    flake derivation to build the .elf's

--- flake.nix
+++ flake.nix
@@ -20,5 +20,29 @@
           # I use STM32_Programmer_CLI to program the option bytes, but no Nix package provides it
         ];
       };
+      
+      packages.${system}.firmware = pkgs.stdenvNoCC.mkDerivation {
+        pname = "stm32l552zeq nucleo firmware";
+        version = "1.0";
+
+        src = self;
+
+        nativeBuildInputs = [
+          pkgs.gcc-arm-embedded
+          pkgs.gnumake
+        ];
+
+        buildPhase = ''
+          make
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp main.elf $out/
+          cp mhs.elf $out/
+          cp secure.elf $out/
+          cp nonsecure.elf $out/
+        '';
+      };
     };
 }
